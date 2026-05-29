b76355b7624638b2f001db853b5b46ca2bd44d9e
BigDuck <bigduck@gmail.com>
2026-05-29 14:05:06 +0800

    backend update

--- build/startup.sh
+++ build/startup.sh
@@ -1,4 +1,4 @@
 docker build -t backendnbc .
 docker stop nbcb1
 docker rm nbc1
-docker run -d -v ~/lognbc:/data/log  --network backend-network-nbc --restart always  --name b1 -p 10888:8888 backendnbc 
+docker run -d -v ~/lognbc:/data/log  --network backend-network-nbc --restart always  --name nbc1 -p 10888:8888 backendnbc 
