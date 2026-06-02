f2dd9eed2c78cf01986ad3bfb55bb56321ccf2ec
BigDuck <bigduck@gmail.com>
2026-06-02 22:07:26 +0800

    backend update

---
+++ front/console/js/pixel-client-legacy.1780409090000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1780409090000 */
+System.register(["../assets/pixel-entry-index-legacy.1780409090000.js"],function(t,e){"use strict";var n;return{setters:[function(t){n=t.a1}],execute:function(){t("v",function(t){return n({url:"/client/verifySetting",method:"post",data:t})}),t("g",function(t){return n({url:"/client/tocp",method:"get",params:t})}),t("c",function(t){return n({url:"/client/tocp",method:"post",data:t})}),t("d",function(t){return n({url:"/client/tocp",method:"delete",data:t})})}}});
---
+++ front/console/js/pixel-index-legacy.178040909000012.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1780409090000 */
+System.register(["../assets/pixel-entry-index-legacy.1780409090000.js"],function(e,n){"use strict";var t,u,r,i,l,a,o,c,s,f,d;return{setters:[function(e){t=e.K,u=e.d,r=e.c,i=e.o,l=e.b,a=e.w,o=e.T,c=e.l,s=e.N,f=e.u,d=e.O}],execute:function(){var n={name:"Profile"};e("default",Object.assign(n,{setup:function(e){var n=t();return function(e,t){var v=u("router-view");return i(),r("div",null,[l(v,null,{default:a(function(e){var t=e.Component;return[l(o,{mode:"out-in",name:"el-fade-in-linear"},{default:a(function(){return[(i(),c(s,{include:f(n).keepAliveRouters},[(i(),c(d(t)))],1032,["include"]))]}),_:2},1024)]}),_:1})])}}}))}}});
