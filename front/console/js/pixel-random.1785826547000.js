cf2576961d03bf61c96b387210af5b5a030d3cff
BigDuck <bigduck@gmail.com>
2026-08-04 15:20:35 +0800

    backend update

---
+++ front/console/js/pixel-random.1785826547000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1785826547000 */
+const a=()=>{return"5".concat(String((a=0,t=9999999,Math.floor(Math.random()*(t-a+1))+a)).padStart(7,"0"));var a,t},t=()=>{const a=(new Date).getFullYear(),t=a-55,r=a-20,o=Math.floor(Math.random()*(r-t+1))+t,n=String(Math.floor(12*Math.random())+1).padStart(2,"0"),c=String(Math.floor(28*Math.random())+1).padStart(2,"0");return"".concat(o,"-").concat(n,"-").concat(c)};export{a,t as r};
