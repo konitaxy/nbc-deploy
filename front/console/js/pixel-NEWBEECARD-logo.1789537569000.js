50acfbaf72fd9c9ff1b7656d65be830704ffcf67
BigDuck <bigduck@gmail.com>
2026-09-23 15:12:19 +0800

    backend update

--- front/console/js/pixel-NEWBEECARD-logo.1789537569000.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on melong-console 
- Time : 1789537569000 */
-const e=""+new URL("../assets/pixel-NEWBEECARD-logo.1789537569000.png",import.meta.url).href;export{e as _};
---
+++ front/console/js/pixel-NEWBEECARD-logo.1790147218000.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on melong-console 
+ Time : 1790147218000 */
+const e=""+new URL("../assets/pixel-NEWBEECARD-logo.1790147218000.png",import.meta.url).href;export{e as _};
